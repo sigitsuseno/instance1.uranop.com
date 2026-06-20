e49bd6a85422a0a9e0a40f7fd134eb32a85a3244
Sigit Suseno <ti.sigitsuseno@gmail.com>
2026-06-20 11:01:55 +0700

    12585

--- temp_excel.php
+++
@@ -1,6 +0,0 @@
-<?php
-require 'vendor/autoload.php';
-use PhpOffice\PhpSpreadsheet\IOFactory;
-$spreadsheet = IOFactory::load('H:/laragon/www/instance1.uranop.com/new_kontrak.xlsx');
-$data = $spreadsheet->getActiveSheet()->toArray();
-print_r(array_slice($data, 0, 5));
--- test_columns.php
+++
@@ -1,7 +0,0 @@
-<?php
-require 'vendor/autoload.php';
-$app = require_once 'bootstrap/app.php';
-$app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
-
-$columns = \Illuminate\Support\Facades\Schema::getColumnListing('bpjs_configs');
-echo json_encode($columns);
