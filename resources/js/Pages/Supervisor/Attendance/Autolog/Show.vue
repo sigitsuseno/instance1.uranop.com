82a78fca81b378be597e3a528a7e32a68f3ec273
Sigit Suseno <ti.sigitsuseno@gmail.com>
2026-06-23 10:26:13 +0700

    4585

--- resources/js/Pages/Supervisor/Attendance/Autolog/Show.vue
+++ resources/js/Pages/Supervisor/Attendance/Autolog/Show.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { ref, onMounted } from 'vue';
 import { useRoute } from 'vue-router';
-import { useApi } from '../../../../../composables/useApi';
+import { useApi } from '../../../../composables/useApi';
 
 const route = useRoute();
 const { get } = useApi();
